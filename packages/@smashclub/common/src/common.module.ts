95898debdf05c7ab2aeeca4c929f634855b6163a
Trandat123 <trannguyendat2003@gmail.com>
2026-02-05 08:34:12 +0700

    feat/SCRUM-22

--- packages/@smashclub/common/src/common.module.ts
+++ packages/@smashclub/common/src/common.module.ts
@@ -2,6 +2,8 @@ import { Module, Global } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GatewayRegistryService } from './services/gateway-registry.service';
 import { LoggingService } from './services/logging.service';
+import { XUserSessionGuard } from './guards/x-usersession.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 /**
  * Common module that provides shared services across all microservices
@@ -14,10 +16,17 @@ import { LoggingService } from './services/logging.service';
  * export class AppModule {}
  * ```
  */
-@Global()
+@Global() 
 @Module({
   imports: [ConfigModule],
-  providers: [GatewayRegistryService, LoggingService],
+  providers: [
+    GatewayRegistryService,
+    LoggingService,
+    {
+      provide:APP_GUARD,
+      useClass:XUserSessionGuard,
+    }
+  ],
   exports: [GatewayRegistryService, LoggingService],
 })
 export class SmashClubCommonModule {}
